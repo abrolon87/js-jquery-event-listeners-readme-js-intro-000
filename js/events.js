62a029c50037297a700b1e3f170d381dbf55bcc6
Amanda R <abrolon87@gmail.com>
2019-08-24 23:44:02 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -12,7 +12,9 @@ function pressIt() {
 }
 
 function submitIt() {
-  
+  if ($( "input:first").val() === "correct") {
+  alert("Your form is going to be submitted now.");
+  }
 }
 $(document).ready(function(){
 
